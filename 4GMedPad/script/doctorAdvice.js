f1d7ce6f084daeae00ec361b504a2507b4464df7
hanguopeng <554876514@qq.com>
2019-10-14 09:14:19 +0800

    去掉医嘱明细弹出页加高

--- 4GMedPad/script/doctorAdvice.js
+++ 4GMedPad/script/doctorAdvice.js
@@ -183,7 +183,7 @@ function openFeeDetails(adviceId) {
             x: 0,
             y: api.winHeight - 300,
             w: api.winWidth,
-            h: api.winHeight-250
+            h: api.winHeight-235
         },
         progress: {
             type: "default",
